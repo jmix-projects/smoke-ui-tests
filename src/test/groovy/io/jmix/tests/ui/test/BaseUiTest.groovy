93f4a5d28a0791c094e373695fc6d66c77fcfb38
Nadezhda Shatalova <n.shatalova@haulmont.com>
2022-02-16 10:57:09 +0400

    remove the AfterEach fixture, add the BeforeAll and AfterAll fixture

--- src/test/groovy/io/jmix/tests/ui/test/BaseUiTest.groovy
+++ src/test/groovy/io/jmix/tests/ui/test/BaseUiTest.groovy
@@ -1,9 +1,12 @@
 package io.jmix.tests.ui.test
 
 import com.codeborne.selenide.Selenide
+import com.codeborne.selenide.WebDriverRunner
 import io.jmix.tests.ui.screen.system.login.LoginScreen
 import io.jmix.tests.ui.screen.system.main.MainScreen
-import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.BeforeAll
+import org.openqa.selenium.Dimension
 
 import static io.jmix.masquerade.Selectors.$j
 
@@ -36,11 +39,24 @@ abstract class BaseUiTest {
         $j(LoginScreen).login(login, password)
     }
 
+    static void maximizeWindowSize() {
+        WebDriverRunner.webDriver.manage().window().setSize(new Dimension(1920, 1080))
+    }
+
     /**
-     * Logout after each test
+     * Login before each class
      */
-    @AfterEach
-    void logout() {
+    @BeforeAll
+    static void beforeAll() {
+        loginAsAdmin()
+        maximizeWindowSize()
+    }
+
+    /**
+     * Logout after each class
+     */
+    @AfterAll
+    static void logoutAfterAll() {
         $j(MainScreen).logout()
     }
 }
