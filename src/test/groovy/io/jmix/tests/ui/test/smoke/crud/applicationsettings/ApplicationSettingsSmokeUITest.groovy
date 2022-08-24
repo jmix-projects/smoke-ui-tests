845f8adfaa6dd97ea813ff684e57843cdba1718f
Afanasevaa36 <a.afanasev@haulmont.com>
2022-08-24 09:47:03 +0300

    Some renaming and update test

--- src/test/groovy/io/jmix/tests/ui/test/smoke/crud/applicationsettings/ApplicationSettingsSmokeUITest.groovy
+++ src/test/groovy/io/jmix/tests/ui/test/smoke/crud/applicationsettings/ApplicationSettingsSmokeUITest.groovy
@@ -7,25 +7,21 @@ import io.jmix.tests.ui.test.BaseUiTest
 import io.jmix.tests.ui.test.utils.helpers.UiHelper
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
-import org.junit.jupiter.api.MethodOrderer
-import org.junit.jupiter.api.Order
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.TestMethodOrder
 
 import static io.jmix.masquerade.Selectors.$j
 import static io.jmix.masquerade.Selectors.byJTestId
 
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class ApplicationSettingsSmokeUITest extends BaseUiTest implements UiHelper {
     public static final String APPLICATION_SETTINGS_CUSTOM_ENTITY = "Custom settings (sales_CustomSettings)"
-    public static final String FIELD_NOTIFICATION_TEXT = "картошка"
-    public static final String FIELD_DEFAULT_GRADE = "testik1"
-    public static final String APPLICATION_SETTINGS_EMPTY_STRING = ""
-    public static final String APPLICATION_SETTINGS_NOTIFICATION = "Custom settings saved successfully"
+    public static final String NOTIFICATION_TEXT_FIELD = "Hi! This is a test text"
+    public static final String DEFAULT_GRADE_FIELD = "A"
+    public static final String APP_SETTINGS_EMPTY_STRING = ""
+    public static final String APP_SETTINGS_NOTIFICATION_TEXT_SAVED = "Custom settings saved successfully"
     public static final String NOTIFICATION_TEXT_FIELD_J_TEST_ID = "notificationText"
     public static final String DEFAULT_GRADE_FIELD_J_TEST_ID = "defaultGrade"
-    public String notifText = ""
-    public String defGrade = ""
+    public String notifTextFieldValue = ""
+    public String defGradeFieldValue = ""
 
     @BeforeEach
     void openApplicationSettingsScreen() {
@@ -33,34 +29,50 @@ class ApplicationSettingsSmokeUITest extends BaseUiTest implements UiHelper {
     }
 
     @Test
-    @Order(1)
-    @DisplayName("Selects an entity and fills in the current value fields")
-    void fillsInTheCurrentValueFields() {
-         notifText = getUniqueName(FIELD_NOTIFICATION_TEXT)
-         defGrade = getUniqueName(FIELD_DEFAULT_GRADE)
+    @DisplayName("Сhecks default values and notification when save values")
+    void checkDefaultValuesAndNotification() {
+        notifTextFieldValue = getUniqueName(NOTIFICATION_TEXT_FIELD)
+        defGradeFieldValue = getUniqueName(DEFAULT_GRADE_FIELD)
         $j(ApplicationSettingsScreen).with {
             selectApplicationSettingsEntity(APPLICATION_SETTINGS_CUSTOM_ENTITY)
-            fillTextField($j(TextField, byJTestId(NOTIFICATION_TEXT_FIELD_J_TEST_ID)), notifText)
-            fillTextField($j(TextField, byJTestId(DEFAULT_GRADE_FIELD_J_TEST_ID)), defGrade)
+            fillTextField($j(TextField, byJTestId(NOTIFICATION_TEXT_FIELD_J_TEST_ID)), notifTextFieldValue)
+            fillTextField($j(TextField, byJTestId(DEFAULT_GRADE_FIELD_J_TEST_ID)), defGradeFieldValue)
             checkDefaultNotifText()
             checkDefaultDefGrade()
             clickButton(saveButtonId)
-            checkNotificationCaption(APPLICATION_SETTINGS_NOTIFICATION)
+            checkNotificationCaption(APP_SETTINGS_NOTIFICATION_TEXT_SAVED)
+            clickButton(closeButtonId)
         }
     }
 
     @Test
-    @Order(2)
-    @DisplayName("Removes current values")
-    void removesCurrentValues() {
+    @DisplayName("Fills and cleans values with page reopening")
+    void fillAndCleanValuesWithPageReopening() {
+        notifTextFieldValue = getUniqueName(NOTIFICATION_TEXT_FIELD)
+        defGradeFieldValue = getUniqueName(DEFAULT_GRADE_FIELD)
         $j(ApplicationSettingsScreen).with {
             selectApplicationSettingsEntity(APPLICATION_SETTINGS_CUSTOM_ENTITY)
-            checkFilledTextField($j(TextField, byJTestId(NOTIFICATION_TEXT_FIELD_J_TEST_ID)), notifText)
-            checkFilledTextField($j(TextField, byJTestId(DEFAULT_GRADE_FIELD_J_TEST_ID)), defGrade)
-            fillTextField($j(TextField, byJTestId(NOTIFICATION_TEXT_FIELD_J_TEST_ID)), APPLICATION_SETTINGS_EMPTY_STRING)
-            fillTextField($j(TextField, byJTestId(DEFAULT_GRADE_FIELD_J_TEST_ID)), APPLICATION_SETTINGS_EMPTY_STRING)
+            fillTextField($j(TextField, byJTestId(NOTIFICATION_TEXT_FIELD_J_TEST_ID)), notifTextFieldValue)
+            fillTextField($j(TextField, byJTestId(DEFAULT_GRADE_FIELD_J_TEST_ID)), defGradeFieldValue)
             clickButton(saveButtonId)
-            checkNotificationCaption(APPLICATION_SETTINGS_NOTIFICATION)
+            clickButton(closeButtonId)
+        }
+        openApplicationSettingsScreen()
+        $j(ApplicationSettingsScreen).with {
+            selectApplicationSettingsEntity(APPLICATION_SETTINGS_CUSTOM_ENTITY)
+            checkFilledTextField($j(TextField, byJTestId(NOTIFICATION_TEXT_FIELD_J_TEST_ID)), notifTextFieldValue)
+            checkFilledTextField($j(TextField, byJTestId(DEFAULT_GRADE_FIELD_J_TEST_ID)), defGradeFieldValue)
+            fillTextField($j(TextField, byJTestId(NOTIFICATION_TEXT_FIELD_J_TEST_ID)), APP_SETTINGS_EMPTY_STRING)
+            fillTextField($j(TextField, byJTestId(DEFAULT_GRADE_FIELD_J_TEST_ID)), APP_SETTINGS_EMPTY_STRING)
+            clickButton(saveButtonId)
+            clickButton(closeButtonId)
+        }
+        openApplicationSettingsScreen()
+        $j(ApplicationSettingsScreen).with {
+            selectApplicationSettingsEntity(APPLICATION_SETTINGS_CUSTOM_ENTITY)
+            checkFilledTextField($j(TextField, byJTestId(NOTIFICATION_TEXT_FIELD_J_TEST_ID)), APP_SETTINGS_EMPTY_STRING)
+            checkFilledTextField($j(TextField, byJTestId(DEFAULT_GRADE_FIELD_J_TEST_ID)), APP_SETTINGS_EMPTY_STRING)
+            clickButton(closeButtonId)
         }
     }
 }
